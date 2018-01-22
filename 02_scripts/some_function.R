7baf289bce147b57fe1e66369f5e0d94892ffcce
jixing475 <jixing475@gmail.com>
2018-01-22 23:08:28 +0800

    add correlation analysis

--- 02_scripts/some_function.R
+++ 02_scripts/some_function.R
@@ -1,178 +1,4 @@
 #some function
-to_factor <- function(df){#from Tmisc::unfactor
-  id <- sapply(df, is.character)
-  df[id] <- lapply(df[id], as.factor)
-  df
-}
-cha_to_fac <- function(df){
-  id <- sapply(df,is.character)
-  df[id] <- lapply(df[id],as.factor)
-  df
-}
-if_impute <- function(x) {
-  if(shapiro.test(x)$p.value > 0.05){
-    x <- impute(x,fun=mean)
-  }
-  else{
-    x <- impute(x,fun=median)
-  }
-  return(x)
-}
-
-to_impute <- function(df){#from Tmisc::unfactor
-  id <- sapply(df, is.numeric)
-  df[id] <- lapply(df[id], FUN=if_impute)
-  df[id] <- lapply(df[id], as.numeric)
-  df
-}
-get_num <- function(df){
-  id <- sapply(df,is.numeric)
-  df[id]
-}
-get_unum <- function(df){#from Tmisc::unfactor
-  id <- sapply(df, is.numeric)
-  df[-id]
-}
-get_fac <- function(df){
-  id <- sapply(df,is.factor)
-  df[id]
-}
-get_p <- function(x,fac,paired=FALSE,digits = 3){
-  if(shapiro.test(x)$p.value >0.05){
-    test<- t.test(x~fac,paired = FALSE)
-  }
-  else{
-    test <- wilcox.test(x~fac,paired = FALSE)
-  }
-  return(signif(test$p.value,digits = digits))
-}
-
-get_stat_num <- function(x,na.omit=TRUE,digits=3){
-  library(stringr)
-  library(dplyr)
-  if(na.omit)
-    x <- x[!is.na(x)]
-  if(shapiro.test(x)$p.value >0.05){
-    m <- signif(mean(x),digits = digits)
-    s <- signif(sd(x),digits = digits)
-    stat <- str_c(m,"±",s)
-  }
-  else{
-    m <- signif(median(x),digits = digits)
-    IQR_1 <- signif(quantile(x)[2],digits = digits)
-    IQR_3 <- signif(quantile(x)[4],digits = digits)
-    stat <- str_c(m,"(",IQR_1,"-",IQR_3,")")
-  }
-  return(stat)
-}
-get_stat_mean <- function(x,na.omit=TRUE,digits=3){
-  library(stringr)
-  library(dplyr)
-  if(na.omit)
-    x <- x[!is.na(x)]
-  m <- signif(mean(x),digits = digits)
-  s <- signif(sd(x),digits = digits)
-  stat <- str_c(m,"±",s)
-  return(stat)
-}
-get_stat_median <- function(x,na.omit=TRUE,digits=3){
-  library(stringr)
-  library(dplyr)
-  if(na.omit)
-    x <- x[!is.na(x)]
-  m <- signif(median(x),digits = digits)
-  IQR_1 <- signif(quantile(x)[2],digits = digits)
-  IQR_3 <- signif(quantile(x)[4],digits = digits)
-  stat <- str_c(m,"(",IQR_1,"-",IQR_3,")")
-  return(stat)
-}
-
-get_stat_fac <- function(x,fac=fac){
-  a <- table(x) %>%
-    prop.table()%>%
-    as.matrix() %>%
-    apply(FUN=signif,digit=3,MARGIN = 2) %>%
-    t() %>%
-    t()*100 
-  a <- as.data.frame(a)
-  a$Freq <- as.data.frame.table(table(x)) %>% .[[2]] 
-  a <- a %>%
-    mutate(total= str_c(Freq,"(",V1,"%",")")) %>%
-    select(total)
-  
-  b_0 <- as.data.frame.table(xtabs(~ x+fac)) %>%
-    rename(count=Freq)
-  b <- 
-    xtabs(~ x+fac) %>%
-    prop.table(.,margin = 2) %>%
-    as.data.frame.table() %>%
-    mutate(Freq= signif(Freq,digit=3)) %>%
-    mutate(Freq= Freq*100) %>%
-    left_join(b_0,by=c("x","fac")) %>%
-    mutate(n_Fre=str_c(count,"(",Freq,"%",")")) %>%
-    select(x,fac,n_Fre) %>%
-    spread( key = fac, value = n_Fre)
-  
-  c <- xtabs(~ x +fac)%>%
-    chisq.test()
-  c <- c$p.value
-  
-  b$Total <- a[[1]]
-  b$p_value <- NA
-  b$p_value[1] <- c
-  b <- select(b,x,Total,everything())
-}
-
-summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
-                      conf.interval=.95, .drop=TRUE) {
-  library(plyr)
-  
-  # New version of length which can handle NA's: if na.rm==T, don't count them
-  length2 <- function (x, na.rm=FALSE) {
-    if (na.rm) sum(!is.na(x))
-    else       length(x)
-  }
-  
-  # This does the summary. For each group's data frame, return a vector with
-  # N, mean, and sd
-  datac <- ddply(data, groupvars, .drop=.drop,
-                 .fun = function(xx, col) {
-                   c(N    = length2(xx[[col]], na.rm=na.rm),
-                     mean = mean   (xx[[col]], na.rm=na.rm),
-                     sd   = sd     (xx[[col]], na.rm=na.rm)
-                   )
-                 },
-                 measurevar
-  )
-  
-  # Rename the "mean" column    
-  datac <- rename(datac, c("mean" = measurevar))
-  
-  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
-  
-  # Confidence interval multiplier for standard error
-  # Calculate t-statistic for confidence interval: 
-  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
-  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
-  datac$ci <- datac$se * ciMult
-  
-  return(datac)
-}
-eGFR_fun <- function(sex,scr,age) {
-  if (sex == "男"){
-    X=min(scr/(88.41*0.9),1)^-0.411
-    Y=max(scr/(88.41*0.9),1)^-1.029
-    Z=0.993^age
-    result <- 141*X*Y*Z 
-  }
-  else{
-    X=min(scr/(88.41*0.7),1)^-0.329
-    Y=max(scr/(88.41*0.7),1)^-1.029
-    Z=0.993^age
-    result <- 141*X*Y*Z*1.018
-  }
-  return(result)
-}
 
 lm_table <- function(y_name, df){
   dat <- df %>%
@@ -194,7 +20,7 @@ lm_table <- function(y_name, df){
     # Merge all columns in one
     library(tidyr)
     table2 = unite(table2,
-                   "95%CI",
+                   "CI",
                    c(Lower, b, Higher),
                    sep = "",
                    remove = T)
@@ -203,4 +29,173 @@ lm_table <- function(y_name, df){
   })
   res <- do.call(rbind, res)
   return(res)
+}
+
+# x is a matrix containing the data
+# method : correlation method. "pearson"" or "spearman"" is supported
+# removeTriangle : remove upper or lower triangle
+# results :  if "html" or "latex"
+# the results will be displayed in html or latex format
+corstars <-function(x, method=c("pearson", "spearman"), removeTriangle=c("upper", "lower"),
+                    result=c("none", "html", "latex")){
+  #Compute correlation matrix
+  require(Hmisc)
+  x <- as.matrix(x)
+  correlation_matrix<-rcorr(x, type=method[1])
+  R <- correlation_matrix$r # Matrix of correlation coeficients
+  p <- correlation_matrix$P # Matrix of p-value 
+  
+  ## Define notions for significance levels; spacing is important.
+  mystars <- ifelse(p < .0001, "****", ifelse(p < .001, "*** ", ifelse(p < .01, "**  ", ifelse(p < .05, "*   ", "    "))))
+  
+  ## trunctuate the correlation matrix to two decimal
+  R <- format(round(cbind(rep(-1.11, ncol(x)), R), 2))[,-1]
+  
+  ## build a new matrix that includes the correlations with their apropriate stars
+  Rnew <- matrix(paste(R, mystars, sep=""), ncol=ncol(x))
+  diag(Rnew) <- paste(diag(R), " ", sep="")
+  rownames(Rnew) <- colnames(x)
+  colnames(Rnew) <- paste(colnames(x), "", sep="")
+  
+  ## remove upper triangle of correlation matrix
+  if(removeTriangle[1]=="upper"){
+    Rnew <- as.matrix(Rnew)
+    Rnew[upper.tri(Rnew, diag = TRUE)] <- ""
+    Rnew <- as.data.frame(Rnew)
+  }
+  
+  ## remove lower triangle of correlation matrix
+  else if(removeTriangle[1]=="lower"){
+    Rnew <- as.matrix(Rnew)
+    Rnew[lower.tri(Rnew, diag = TRUE)] <- ""
+    Rnew <- as.data.frame(Rnew)
+  }
+  
+  ## remove last column and return the correlation matrix
+  Rnew <- cbind(Rnew[1:length(Rnew)-1])
+  if (result[1]=="none") return(Rnew)
+  else{
+    if(result[1]=="html") print(xtable(Rnew), type="html")
+    else print(xtable(Rnew), type="latex") 
+  }
+} 
+
+# ++++++++++++++++++++++++++++
+# flattenCorrMatrix
+# ++++++++++++++++++++++++++++
+# cormat : matrix of the correlation coefficients
+# pmat : matrix of the correlation p-values
+flattenCorrMatrix <- function(cormat, pmat) {
+  ut <- upper.tri(cormat)
+  data.frame(
+    row = rownames(cormat)[row(cormat)[ut]],
+    column = rownames(cormat)[col(cormat)[ut]],
+    cor  =(cormat)[ut],
+    p = pmat[ut]
+  )
+}
+
+#+++++++++++++++++++++++++
+# Computing of correlation matrix
+#+++++++++++++++++++++++++
+# Required package : corrplot
+# x : matrix
+# type: possible values are "lower" (default), "upper", "full" or "flatten";
+#display lower or upper triangular of the matrix, full  or flatten matrix.
+# graph : if TRUE, a correlogram or heatmap is plotted
+# graphType : possible values are "correlogram" or "heatmap"
+# col: colors to use for the correlogram
+# ... : Further arguments to be passed to cor or cor.test function
+# Result is a list including the following components :
+# r : correlation matrix, p :  p-values
+# sym : Symbolic number coding of the correlation matrix
+rquery.cormat<-function(x,
+                        type=c('lower', 'upper', 'full', 'flatten'),
+                        graph=TRUE,
+                        graphType=c("correlogram", "heatmap"),
+                        col=NULL, ...)
+{
+  library(corrplot)
+  # Helper functions
+  #+++++++++++++++++
+  # Compute the matrix of correlation p-values
+  cor.pmat <- function(x, ...) {
+    mat <- as.matrix(x)
+    n <- ncol(mat)
+    p.mat<- matrix(NA, n, n)
+    diag(p.mat) <- 0
+    for (i in 1:(n - 1)) {
+      for (j in (i + 1):n) {
+        tmp <- cor.test(mat[, i], mat[, j], ...)
+        p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+      }
+    }
+    colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
+    p.mat
+  }
+  # Get lower triangle of the matrix
+  getLower.tri<-function(mat){
+    upper<-mat
+    upper[upper.tri(mat)]<-""
+    mat<-as.data.frame(upper)
+    mat
+  }
+  # Get upper triangle of the matrix
+  getUpper.tri<-function(mat){
+    lt<-mat
+    lt[lower.tri(mat)]<-""
+    mat<-as.data.frame(lt)
+    mat
+  }
+  # Get flatten matrix
+  flattenCorrMatrix <- function(cormat, pmat) {
+    ut <- upper.tri(cormat)
+    data.frame(
+      row = rownames(cormat)[row(cormat)[ut]],
+      column = rownames(cormat)[col(cormat)[ut]],
+      cor  =(cormat)[ut],
+      p = pmat[ut]
+    )
+  }
+  # Define color
+  if (is.null(col)) {
+    col <- colorRampPalette(
+      c("#67001F", "#B2182B", "#D6604D", "#F4A582",
+        "#FDDBC7", "#FFFFFF", "#D1E5F0", "#92C5DE", 
+        "#4393C3", "#2166AC", "#053061"))(200)
+    col<-rev(col)
+  }
+  
+  # Correlation matrix
+  cormat<-signif(cor(x, use = "complete.obs", ...),2)
+  pmat<-signif(cor.pmat(x, ...),2)
+  # Reorder correlation matrix
+  ord<-corrMatOrder(cormat, order="hclust")
+  cormat<-cormat[ord, ord]
+  pmat<-pmat[ord, ord]
+  # Replace correlation coeff by symbols
+  sym<-symnum(cormat, abbr.colnames=FALSE)
+  # Correlogram
+  if(graph & graphType[1]=="correlogram"){
+    corrplot(cormat, type=ifelse(type[1]=="flatten", "lower", type[1]),
+             tl.col="black", tl.srt=45,col=col,...)
+  }
+  else if(graphType[1]=="heatmap")
+    heatmap(cormat, col=col, symm=TRUE)
+  # Get lower/upper triangle
+  if(type[1]=="lower"){
+    cormat<-getLower.tri(cormat)
+    pmat<-getLower.tri(pmat)
+  }
+  else if(type[1]=="upper"){
+    cormat<-getUpper.tri(cormat)
+    pmat<-getUpper.tri(pmat)
+    sym=t(sym)
+  }
+  else if(type[1]=="flatten"){
+    cormat<-flattenCorrMatrix(cormat, pmat)
+    pmat=NULL
+    sym=NULL
+  }
+  list(r=cormat, p=pmat, sym=sym)
 }
